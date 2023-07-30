b1c100fb4e795672d28529fef646fd310a609eb7
Farhan Zaman Ha-mim <65287208+FarhanHamim@users.noreply.github.com>
2023-07-30 22:14:13 +0600

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,22 +1,21 @@
 <?php
-// Connect to your database (replace 'your_host', 'your_username', 'your_password', and 'your_database' with your actual credentials)
 $host = 'localhost';
 $username = 'root';
 $password = '';
 $database = 'contact_form';
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check the connection
+// Checking connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get the form data
+// gettiing data from form
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-// Prepare and execute the SQL query to insert the data into the database
+// Preparing and execute the SQL query to inserting the data into the database
 $sql = "INSERT INTO contact (name, email, message) VALUES ('$name', '$email', '$message')";
 
 if ($conn->query($sql) === TRUE) {
